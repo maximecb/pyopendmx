214271b161080abe22243e50e688777512e48fcc
Maxime Chevalier-Boisvert <maximechevalierb@gmail.com>
2021-04-02 16:05:17 -0400

    Got DMX output working with OpenDMX and simple fixture

--- test.py
+++ test.py
@@ -1,14 +1,92 @@
-import pyftdi.serialext
+import time
+import math
+#import pyftdi.serialext
 
-# S/N: AL6E8JFW
-
-# Open a serial port on the second FTDI device interface (IF/1)
-port = pyftdi.serialext.serial_for_url('ftdi://ftdi:2232h/1', baudrate=250000)
-
+from pyftdi.ftdi import Ftdi
 
 
 
 
+port = Ftdi.create_from_url('ftdi://ftdi:232:AL6E8JFW/1')
+
+
+#open_bitbang_from_url(url, direction=0, latency=16, baudrate=1000000, sync=False)
+#open_bitbang(vendor, product, bus=None, address=None, index=0, serial=None, interface=1, direction=0, latency=16, baudrate=1000000, sync=False)
+
+
+port.set_baudrate(baudrate=250000)
+port.set_line_property(bits=8, stopbit=2, parity='N', break_=False)
+
+
+
+assert port.is_connected
+
+
+
+
+# The 0th byte must be 0 (start code)
+# 512 bytes are sent in total
+data = bytearray(513 * [0])
+
+
+data[1] = 255
+data[5] = 255
+data[6] = 255
+data[7] = 255
+data[8] = 255
+
+
+itr = 0
+
+
+
+while True:
+    print('send')
+
+    cyclepos = (itr % 50) / 50.0
+    val = (math.sin(2 * math.pi * cyclepos) + 1) / 2
+
+    # Strobe line
+    data[1] = int(val * 255)
+
+
+    port.set_break(True)
+    port.set_break(False)
+    port.write_data(data)
+
+
+
+    time.sleep(20/1000.0)
+    itr += 1
+
+
+
+
+port.close()
+
+
+
+
+
+
+# Send bytes
+#port.write(b'Hello World')
+
+
+
+
+
+
+
+
+
+
+
+
+#import pyftdi.ftdi
+#ftdi = pyftdi.ftdi.Ftdi()
+#print(ftdi.show_devices())
+#ftdi://ftdi:232:AL6E8JFW/1
 
 #ftDCB.BaudRate = 250000;
 #status = FT_SetDataCharacteristics(handle, BITS_8, STOP_BITS_2, PARITY_NONE);
@@ -20,12 +98,13 @@ port = pyftdi.serialext.serial_for_url('ftdi://ftdi:2232h/1', baudrate=250000)
 
 
 
+#'ftdi://ftdi:232/1',
+#'ftdi://ftdi:232:AL6E8JFW/1',
+#baudrate=250000,
+#bytesize=8,
+#stopbits=2,
+#parity='N',
+#xonxoff=0,
+#rtscts=0
 
-# The 0th byte must be 0 (start code)
-# 512 bytes are sent in total
 
-# Send bytes
-#port.write(b'Hello World')
-
-
-# Thread.Sleep(20);
